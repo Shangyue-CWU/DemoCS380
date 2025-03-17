dc4b22ff066676fc7d8c9f02a3536fc8c55e4ad8
Shangyue-CWU <zhush@cwu.edu>
2025-03-17 11:38:19 -0700

    the 1st Cat.java

---
+++ Cat.java
@@ -0,0 +1,17 @@
+public class TestSuite {
+	public static void main(String[] args) {
+        System.out.println("Hello, Cat!");
+
+    }
+
+}
+
+public class Cat{
+    private String name;
+    private int age;
+    // This is a Cat function
+    public Cat(String name, int age){
+        this.age = age;
+        this.name = name;
+    }
+}
